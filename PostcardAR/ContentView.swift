1f389da7a744168e6a258f59a3635c0530764042
your name <fitraramadhan668@gmail.com>
2026-08-15 00:40:55 +0800

    Initial Commit

---
+++ PostcardAR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PostcardAR
+//
+//  Created by Fitra Ramadhan on 15/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PostcardAR/PostcardARApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostcardARApp.swift
+//  PostcardAR
+//
+//  Created by Fitra Ramadhan on 15/08/26.
+//
+
+import SwiftUI
+
+@main
+struct PostcardARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
